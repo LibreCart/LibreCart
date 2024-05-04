9cb66f5a394b4871238cdc9b40f54c6eaff2d12c
mvmenno <mennovanmuilwijk@gmail.com>
2024-05-04 11:57:49 +0200

    Fix unit test database transactions with rollback support

--- tests/BaseKernelTest.php
+++ tests/BaseKernelTest.php
@@ -3,12 +3,36 @@ namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
+
 class BaseKernelTest extends KernelTestCase {
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->bootKernel();
-        $this->markTestIncomplete();
+
+        $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $entityManager->getConnection()->setNestTransactionsWithSavepoints(true);
+        $entityManager->beginTransaction();
+    }
+
+    protected function tearDown(): void
+    {
+        $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+
+        if ($entityManager->getConnection()->isTransactionActive()) {
+            $entityManager->rollback();
+        }
+
+        $entityManager->close();
+        parent::tearDown();
+    }
+
+    public function testCanConnectToDatabase() {
+        $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+
+        $connection = $entityManager->getConnection();
+
+        $this->assertNotEmpty($connection->fetchAllAssociative('SELECT 1'));
     }
 }
